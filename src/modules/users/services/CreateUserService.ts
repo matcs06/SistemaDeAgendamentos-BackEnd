7cbd48a479b9a9dbb4be1915ec12d739bdddbb9b
Mateus Costa Silva <matcs06@gmail.com>
2022-02-05 20:49:00 -0300

    adding user create security

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,11 +1,13 @@
 import {inject, injectable} from "tsyringe"
 import { IUserRepository } from '../repositories/IUserRepository';
 import { hash } from "bcrypt"
+import verifyKey from "../../../utils/UserSecurity"
 
 interface IRequest{
    name:string;
    username:string;
    password: string;
+   key:string;
 }
 
 @injectable()
@@ -17,13 +19,15 @@ class CreateUserService {
   }
 
   async execute({
-    name,password,username
+    name,password,username, key
   }:IRequest):Promise<void> {
     const userAlreadyExists = await this.UserRepository.findByName(username)
 
     if(userAlreadyExists){
       throw new Error("User already exists")
     }
+
+    verifyKey(key)
   
     const passwordHash = await hash(password, 8)
 
---
+++ src/utils/UserSecurity.ts
@@ -0,0 +1,8 @@
+import { AppError } from "../shared/errors/AppError"
+
+export default function verifyKey(key:string){
+   const createUserKey = "4c176028a04a101cba2d36ca5cbb1155"
+   if(key != createUserKey){
+      throw new AppError("Invalid key to create user")
+   }
+}
